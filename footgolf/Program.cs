39ebc90478fb524cfb414b80d25fd9c838af7656
Csárdás Ferenc <csardas.f.l@icloud.com>
2020-03-30 11:51:54 +0200

    base

---
+++ footgolf/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    class Program
+    {
+        static List<Versenyzo> Mezony = new List<Versenyzo>();
+        static void Main(string[] args)
+        {
+            F02();
+            F03();
+            F04();
+            F06();
+            F07();
+            F08();
+            Console.ReadKey();
+        }
+
+        private static void F08()
+        {
+            var egyesuletek = Mezony.GroupBy(x => x.Egyesulet).Select(x => new { EgyesuletNev = x.Key, EgyesuletCount = x.Count() }).ToList();
+            for (int i = egyesuletek.Count - 1; i > 0; i--)
+                if (egyesuletek[i].EgyesuletCount <= 2 || egyesuletek[i].EgyesuletNev == "n.a.") egyesuletek.Remove(egyesuletek[i]);
+            Console.WriteLine("F08: Egyesület statisztika: ");
+            foreach (var t in egyesuletek)
+                Console.WriteLine($"\t{t.EgyesuletNev} - {t.EgyesuletCount} fő");
+
+        }
+
+        private static void F07()
+        {
+            StreamWriter sw = new StreamWriter(@"..\..\Res\oszpontFF.txt", false, Encoding.UTF8);
+            var ffversenyzok = Mezony.Where(x => x.Kategoria == true);
+            foreach (var f in ffversenyzok)
+                sw.WriteLine($"{f.Nev};{f.Eredmeny()}");
+            sw.Close();
+        }
+
+        private static void F06()
+        {
+            var legjobb = Mezony.Where(x => x.Kategoria == false).OrderByDescending(x => x.Eredmeny()).FirstOrDefault();
+            Console.WriteLine($"F06: A bajnok női versenyző:\n\tNév: {legjobb.Nev}\n\tEgyesület: {legjobb.Egyesulet}\n\tÖsszpont: {legjobb.Eredmeny()}");
+        }
+
+        private static void F04()
+        {
+            Console.WriteLine($"F04: A női versenyzők aránya: {(((double)Mezony.Count(x => x.Kategoria == false) / Mezony.Count) * 100).ToString("0.00")}%");
+        }
+
+        private static void F03()
+        {
+            Console.WriteLine($"F03: Versenyzők száma: {Mezony.Count}");
+        }
+
+        private static void F02()
+        {
+            StreamReader sr = new StreamReader(@"..\..\Res\fob2016.txt", Encoding.UTF8);
+            while (!sr.EndOfStream) Mezony.Add(new Versenyzo(sr.ReadLine()));
+            Console.WriteLine("F02: Adatok feltöltve.");
+            sr.Close();
+        }
+    }
+}
---
+++ footgolf/Versenyzo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    class Versenyzo
+    {
+        public Versenyzo(string s)
+        {
+            var loc = s.Split(';');
+            Nev = loc[0];
+            Kategoria = loc[1] == "Felnott ferfi";
+            Egyesulet = loc[2];
+            Pontok = new int[8];
+            for (int i = 3; i < loc.Length; i++) Pontok[i - 3] = int.Parse(loc[i]);
+        }
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int[] Pontok { get; set; }
+        public int Eredmeny()
+        {
+            Pontok = Pontok.OrderByDescending(x => x).ToArray();
+            int pontszam = Pontok.Sum();
+            for (int i = 6; i < Pontok.Length; i++)
+            {
+                if (Pontok[i] != 0) pontszam -= (Pontok[i] - 10);
+            }
+            return pontszam;
+        }
+    }
+}
